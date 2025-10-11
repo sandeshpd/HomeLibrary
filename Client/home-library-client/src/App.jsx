e1bb1e8f2bd98c90ccd19ea94bd10dd7636e642b
sandeshpd <sandesh@sandesh.com>
2025-10-11 22:34:48 +0530

    Initial commit

---
+++ Client/home-library-client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hello World from React!
+      </h1>
+    </>
+  )
+}
+
+export default App;
---
+++ Client/home-library-client/src/components/book/Book.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Book({book}) {
+  return (
+    <>
+    {/* YouTube paused at 9:05:29 */}
+    </>
+  )
+}
+
+export default Book;
