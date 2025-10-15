932762efd1b95ddcbb7b198bcc013438bb17d2f0
sandeshpd <sandesh@sandesh.com>
2025-10-15 22:27:24 +0530

    Added routing

--- Client/home-library-client/src/App.jsx
+++ Client/home-library-client/src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import List from "./pages/List";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center">
-        Hello World from React!
-      </h1>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/list" element={<List />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
