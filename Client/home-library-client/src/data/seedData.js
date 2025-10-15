57be3ee0e7f65b8e012dc2dc3acc360ef86215e9
sandeshpd <sandesh@sandesh.com>
2025-10-15 22:26:29 +0530

    Hard coded seeded data

---
+++ Client/home-library-client/src/data/seedData.js
@@ -0,0 +1,92 @@
+export const headers = [
+    "Title",
+    "Author",
+    "Price",
+    "Language",
+    "Publication",
+    "Genre",
+    "Actions"
+];
+
+export const seed = [
+    {
+        "book_id": "1",
+        "title": "Chetkin",
+        "author": "Narayan Dharap",
+        "price": 200,
+        "language": "Marathi",
+        "publication": "Saket Prakashan",
+        "genre": [
+            {
+                "genre_id": 1,
+                "genre_name": "Horror"
+            }
+        ],
+        "actions": "edit and delete"
+    },
+    {
+        "book_id": "2",
+        "title": "440 Chandanwadi",
+        "author": "Narayan Dharap",
+        "price": 200,
+        "language": "Marathi",
+        "publication": "Dilipraj Prakashan",
+        "genre": [
+            {
+                "genre_id": 1,
+                "genre_name": "Horror"
+            }
+        ],
+        "actions": "edit and delete"
+    },
+    {
+        "book_id": "3",
+        "title": "Shodh",
+        "author": "Narayan Dharap",
+        "price": 250,
+        "language": "Marathi",
+        "publication": "Saket Prakashan",
+        "genre": [
+            {
+                "genre_id": 1,
+                "genre_name": "Horror"
+            },
+            {
+                "genre_id": 8,
+                "genre_name": "Mystery"
+            }
+        ],
+        "actions": "edit and delete"
+    },
+    {
+        "book_id": "4",
+        "title": "Hasanaval",
+        "author": "D. M. Mirasdar",
+        "price": 100,
+        "language": "Marathi",
+        "publication": "Mehta Publishing",
+        "genre": [
+            {
+                "genre_id": 3,
+                "genre_name": "Comedy"
+            }
+        ],
+        "actions": "edit and delete"
+    },
+    {
+        "book_id": "5",
+        "title": "Pathlag",
+        "author": "Narayan Dharap",
+        "price": 100,
+        "language": "Marathi",
+        "publication": "Saket Prakashan",
+        "genre": [
+            {
+                "genre_id": 4,
+                "genre_name": "Crime"
+            }
+        ],
+        "actions": "edit and delete"
+    }
+];
+
